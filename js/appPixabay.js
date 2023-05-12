ef9964e0763229f36cb3d7268a7c51fa7a5ec831
silvamariad <mangel2005@hotmail.com>
2023-05-11 23:27:40 -0500

    actualizado 2.2

--- js/appPixabay.js
+++ js/appPixabay.js
@@ -26,29 +26,10 @@ const cargarImagenes=async()=>{
     imagenes.map(imagen=>{
                         const{largeImageURL, likes, previewURL, tags, views}=imagen;
 
-        /**como ordenar las img 
-         * <div class="row row-cols-md-3 mb-3 text-center">
-                                <div class="col">
-                                    <div class="card mb-3 rounded-3 shadow-sm">
-                                        <div class="card-header py-3">
-                                            <img src="${previewURL}" alt="${tags}" class="card-img-top">
-
-                                            <div class="card-body">
-                                                <p class="card-text">${likes} Me gusta </p>
-                                                <p class="card-text">${views} Visitas </p>
-                                            </div>
-
-                                            <div class="card-footer">
-                                                <a href="${largeImageURL}" target="_blank" rel="noopener noreferrer" class="btn btn-primary btn-block"> Ver Imagen </a> 
-                                            </div>
-                                        </div>
-                                    </div> 
-                                </div>
-                            </div>
-        * **/
+        /**como ordenar las img **/
             imagenesHTML+=` 
                         <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
-                            <div class="card">
+                            <div class="card-group">
                                     <img src="${previewURL}" alt="${tags}" class="card-img-top">
                                 <div class="card-body">
                                     <p class="card-text">${likes} Me gusta </p>
