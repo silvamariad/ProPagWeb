c6e7d735a1d3696d19938ea2c4d7e2033db0320e
silvamariad <mangel2005@hotmail.com>
2023-05-15 23:36:13 -0500

    actualizado 2.6

---
+++ js/formulario.js
@@ -0,0 +1,7 @@
+const expresiones = {
+    usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, //letras, nuemeros, guion y guion_bajo.
+    usuario: /^[a-zA-ZÁ-ý\s]{1,48}$/, //letras y espacios, pueden llevar acentos.
+    password: /^.{4,12}$/, // 4 a 12 digitales.
+    correo: /^[a-zA-Z0-9_.+]+@[a-zA-Z0-9]+\.[a-zA-Z0-9-.]+$/, //validacion para el campo del correo
+    telefono: /^\d{7,10}$/ //7 a 10 numeros
+}
