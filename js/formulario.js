c39a08b4b587606080c7cee9b19578155b2021bc
silvamariad <mangel2005@hotmail.com>
2023-05-20 22:59:19 -0500

    actualizado 3.0

--- js/formulario.js
+++ js/formulario.js
@@ -9,7 +9,56 @@ const expresiones = {
     correo  : /^[a-zA-Z0-9_.+]+@[a-zA-Z0-9]+\.[a-zA-Z0-9-.]+$/, //validacion para el campo del correo
     telefono: /^\d{7,10}$/ //7 a 10 numeros
 }
+/** Validacion de los campos **/
+const validarFormulario = (e) => {
+    switch (e.target.name) {
+        case "usuario":
+            if(expresiones.usuario.test(e.target.value)){
+                document.getElementById('grupo__usuario').classList.remove('formulario__grupo-incorrecto');
+                document.getElementById('grupo__usuario').classList.add('formulario__grupo-correcto');
+                document.querySelector('#grupo__usuario i').classList.remove('fa-times-circle');
+                document.querySelector('#grupo__usuario i').classList.add('fa-check-circle');
+                document.querySelector('#grupo__usuario .formulario__input-error').classList.remove('formulario__input-error');
+            } else { 
+                document.getElementById('grupo__usuario').classList.add('formulario__grupo-incorrecto');
+                document.getElementById('grupo__usuario').classList.remove('formulario__grupo-correcto');
+                document.querySelector('#grupo__usuario i').classList.add('fa-times-circle');
+                document.querySelector('#grupo__usuario i').classList.remove('fa-check-circle');
+                document.querySelector('#grupo__usuario .formulario__input-error').classList.add('formulario__input-error');
+            }
+        break;
 
-formulario.addEventListener('submit', () => {
-    
+        case "nombre":
+        break;
+
+        case "tipdoc":
+        break;
+
+        case "nrodoc":
+        break;
+
+        case "usuario":
+        break;
+
+        case "password":
+        break;
+
+        case "password2":
+        break;
+
+        case "correo":
+        break;
+
+        case "telefono":
+        break;
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', validarFormulario);
+    input.addEventListener('blur', validarFormulario);
+});
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault(); 
 });
