1ef0061a4609aaae7c27432700576804e0608b55
silvamariad <mangel2005@hotmail.com>
2023-05-25 21:20:33 -0500

    actualizado 3.2

--- js/formulario.js
+++ js/formulario.js
@@ -4,6 +4,7 @@ const inputs = document.querySelectorAll('#formulario input');
 const expresiones = {
     usuario : /^[a-zA-Z0-9\_\-]{4,16}$/, //letras, nuemeros, guion y guion_bajo.
     nombre  : /^[a-zA-ZÁ-ý\s]{1,48}$/, //letras y espacios, pueden llevar acentos.
+    tipdoc  : /^[a-zA-ZÁ-ý\s]{1,48}$/,
     nrodoc  : /^[a-zA-ZÁ-ý\s]{1,48}$/, //letras y espacios, pueden llevar acentos.
     password: /^.{4,12}$/, // 4 a 12 digitales.
     correo  : /^[a-zA-Z0-9_.+]+@[a-zA-Z0-9]+\.[a-zA-Z0-9-.]+$/, //validacion para el campo del correo
@@ -21,30 +22,33 @@ const validarFormulario = (e) => {
         break;
 
         case "tipdoc":
+            validarCampo(expresiones.tipdoc, e.target, 'tipdoc');
         break;
 
         case "nrodoc":
-        break;
-
-        case "usuario":
+            validarCampo(expresiones.nrodoc, e.target, 'nrodoc');
         break;
 
         case "password":
+            validarCampo(expresiones.password, e.target, 'password');
         break;
 
         case "password2":
+            validarCampo(expresiones.password, e.target, 'password');
         break;
 
         case "correo":
+            validarCampo(expresiones.correo, e.target, 'correo');
         break;
 
         case "telefono":
+            validarCampo(expresiones.telefono, e.target, 'telefono');
         break;
     }
 }
 
 const validarCampo = (expresion, input, campo) => {
-    if(expresion.test(input.value)){
+    if(expresion.test(e.target.value)){
         document.getElementById (`grupo__${campo}`      ).classList.remove('formulario__grupo-incorrecto');
         document.getElementById (`grupo__${campo}`      ).classList.add('formulario__grupo-correcto');
         document.querySelector  (`#grupo__${campo} i`   ).classList.add('fa-check-circle');
