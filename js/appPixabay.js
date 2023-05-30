1706ed89dde601ea5f24f633d2acac9a98416c68
silvamariad <mangel2005@hotmail.com>
2023-05-30 17:51:09 -0500

    actualizado 4.5

--- js/appPixabay.js
+++ js/appPixabay.js
@@ -13,7 +13,7 @@ const cargarImagenes=async()=>{
     }
 
     /**variable constante para la appPixabay **/
-    const imaginesPorPagina=20;
+    const imagenesPorPagina=20;
     const key="36197946-5703d23ead3a4135af9d516d8";
     const url=`https://pixabay.com/api/?key=${key}&q=${input}&per_page=${imagenesPorPagina}&page=${paginaActual}`;
     console.log(url);
