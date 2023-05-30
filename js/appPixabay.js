2fa1986f9ad891203c4ea1c53a81de86fa986bd7
silvamariad <mangel2005@hotmail.com>
2023-05-30 17:42:16 -0500

    actualizado 4.5

--- js/appPixabay.js
+++ js/appPixabay.js
@@ -23,7 +23,7 @@ const cargarImagenes=async()=>{
 
     /** Respùesta de las img**/
     let imagenes=resultado.hits;
-    console.log(imagenes);
+    //console.log(imagenes);
 
     /** como traer las img del json **/
     let imagenesHTML=``;
